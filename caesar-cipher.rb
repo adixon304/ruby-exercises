f4ce3a1d2b05320f7e144d17063f63a26731b9ca
Alex Dixon <alexdixon15@gmail.com>
2021-09-20 21:05:28 -0400

    Added comments of exercise to all

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,6 @@
+#Caesar Cipher Exercise
+#Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string.
+
 def caesar_cipher(text, key)
 
     #ensure sizing is consistent and split text into array
